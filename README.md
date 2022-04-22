ebfe4361cf80e6aaf5fa1f9d41e00771d5243545
Marco Guido <guido-torres@hotmail.com>
2022-04-22 09:13:46 -0400

    last changes

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # movie-database
-sample movie database interface with mysql,  ejs and more
+
+sample movie database interface with mysql, ejs and more
+
+## How to run?
+
+install required packages
+
+```
+npm i
+```
+
+use the embedded start script
+
+```
+npm start
+```
--- app.js
+++ app.js
@@ -1,17 +1,79 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const mysql = require("mysql");
+const { route } = require("express/lib/router");
 const router = express.Router();
 const app = express();
 const SERVER_PORT = 3000;
+const DATABASE_PORT = 3306;
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const connection = mysql.createConnection({
+  user: "root",
+  password: "root",
+  host: "localhost",
+  port: DATABASE_PORT,
+  database: "nodejs",
+});
+
 router.get("/", function (req, res) {
-  res.render("index");
+  //const colors = [{ name: "red" }, { name: "blue" }, { name: "green" }];
+  const queryString = "SELECT * FROM colours";
+  connection.query(queryString, (err, result) => {
+    if (err) {
+      console.error("Error while querying database");
+    } else {
+      res.render("index", { data: result });
+    }
+  });
+});
+
+router.get("/add-colour", function (req, res) {
+  res.render("add-colour");
+});
+
+router.get("/colour/:id", function (req, res) {
+  const queryString = `SELECT * FROM colours WHERE id = ${req.params.id}`;
+  connection.query(queryString, (err, result) => {
+    if (err) {
+      console.error("Error while querying database");
+    } else {
+      res.render("colour", { data: result[0] });
+    }
+  });
+});
+
+router.post("/update-colour", function (req, res) {
+  const queryString = `UPDATE colours SET name = '${req.body.colourName}' WHERE id = '${req.body.id}'`;
+  console.log(queryString);
+  connection.query(queryString);
+  res.writeHead(302, {
+    Location: "/",
+  });
+  res.end();
+});
+
+router.post("/delete-colour", function (req, res) {
+  const queryString = `DELETE FROM colours WHERE id = '${req.body.id}'`;
+  console.log(queryString);
+  connection.query(queryString);
+  res.writeHead(204);
+  res.end();
+});
+
+router.post("/add-colour-submit", function (req, res) {
+  console.log("data", req.body.colourName);
+  const name = req.body.colourName;
+  const queryString = `INSERT INTO colours (name) VALUES ('${name}')`;
+  connection.query(queryString);
+  res.writeHead(302, {
+    Location: "/",
+  });
+  res.end();
 });
 
 app.use("/", router);
